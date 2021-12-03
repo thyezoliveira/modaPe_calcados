54812734a58ffbf80eff47f2c95311204e4dac1a
Thyéz Oliveira <thyezoliveira@gmail.com>
2021-12-03 10:34:04 -0300

    Working app v0.2.0

--- modape/app.py
+++ modape/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, session,url_for
 from flask.helpers import url_for
 
 app = Flask(__name__)
-
+app.secret_key = '7a927480dcd9c2cd77caba2781b4aacd0f04fc4c683f0a9f4e39f94b1c00a170'
 # classe de usuário -----------------------------------------------------
 class Usuario:
     def __init__(self, username, password):
@@ -19,10 +19,14 @@ usuario1 = Usuario('modape', 'calcados2022')
 #------------------------------------X------------------------------------
 
 # Rota da Tela de login --------------------------------------------------
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
+#@templated()
 def index():
     style = url_for('static', filename='style.css')
+    if 'username' in session:
+        return render_template('dashboard.html', username=session['username'], style=style)
     return render_template('index.html', style=style)
+    # return dict(style=style)
 
 #-----------------------------------X------------------------------------
 
@@ -34,7 +38,7 @@ def register():
 
     if username == usuario1.get_username():
         if password == usuario1.get_password():
-
+            session['username'] = username
             return redirect(url_for('main'), code=307, Response=None)
         else:
             return "Senha incorreta!"
@@ -47,7 +51,7 @@ def register():
 @app.route('/dashboard', methods=['POST'])
 def main():
     style = url_for('static', filename='style.css')
-    return render_template('dashboard.html', name=usuario1.get_username(), style=style)
+    return render_template('dashboard.html', username=session['username'], style=style)
 
 # -----------------------------------X------------------------------------
 
@@ -57,6 +61,14 @@ def products():
     style = url_for('static', filename='style.css')
     return render_template('products.html', name=usuario1.get_username(), style=style)
 # ------------------------------------------------------------------------
+
+# Rota para Sair ------------------------------------------------------
+@app.route('/sair', methods=['GET'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'), code=307, Response=None)
+
+# -----------------------------------X------------------------------------
 
 # Ativar aplicação ao executar o arquivo----------------------------------
 if __name__ == '__main__':
